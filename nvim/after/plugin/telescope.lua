78e85334d19f33c002a6be84cdce3fc0d94c6378
Aleksa <aleksamiskin@gmail.com>
2023-12-03 13:38:59 +0100

    Clarify git search purpose

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,7 +1,7 @@
 local builtin = require('telescope.builtin')
 -- Find files in root directory --
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
--- Find files in git directory --
+-- Find files in git directory, reducing scope of search reduces time to find files --
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 -- Find files containing input string --
 vim.keymap.set('n', '<leader>ps', function()
